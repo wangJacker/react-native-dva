0fe6f2f2c3b5eed35fa04f7640328033cfe3ef5e
PS-1\Admin <wy>
2019-06-21 10:19:17 +0800

    添加初始模版

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+// import { name as appName } from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import './app/index'
+
+console.ignoredYellowBox = [
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Warning: componentWillUpdate is deprecated',
+  'Warning: isMounted(...) is deprecated',
+]
