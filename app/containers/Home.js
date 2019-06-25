99fdd5502ed771435ef19d2ff66791c631f27370
PS-1\Admin <wy>
2019-06-25 19:09:18 +0800

    添加轮播

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,32 +1,52 @@
 import React, { Component } from 'react'
-import { View, Text, Image, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, ActivityIndicator, ScrollView, Platform } from 'react-native'
 import { connect } from 'react-redux'
-
-import { Button } from '../components'
+import { Card, ListItem, Button, CheckBox, Image, Input, Overlay, PricingCard, Rating, AirbnbRating, SocialIcon } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+// import { Button } from '../components'
 import { ScaledSheet, scale } from 'react-native-size-matters/extend';
 import { NavigationActions } from '../utils'
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+// import { sliderWidth, itemWidth } from 'example/src/styles/SliderEntry.style';
+import { sliderWidth, itemWidth } from '../styles/SliderEntry.style';
+// import SliderEntry from 'example/src/components/SliderEntry';
+import SliderEntry from '../components/SliderEntry';
+// import styles, { colors } from 'example/src/styles/index.style';
+import styles, { colors } from '../styles/index.style';
+// import { ENTRIES1, ENTRIES2 } from 'example/src/static/entries';
+import { ENTRIES1, ENTRIES2 } from '../static/entries';
+// import { scrollInterpolators, animatedStyles } from 'example/src/utils/animations';
+import { scrollInterpolators, animatedStyles } from '../utils/animations';
 
+const IS_ANDROID = Platform.OS === 'android';
+const SLIDER_1_FIRST_ITEM = 1;
 @connect()
 class Home extends Component {
 
-    state = {}
+    state = {
+        users: [
+            {
+                name: 'brynn',
+                avatar: 'http://pic15.nipic.com/20110629/5078207_164705330000_2.jpg'
+            },
+        ],
+        checked: true,
+        isVisible: false,
+        slider1ActiveSlide: 1
+
+    }
     static navigationOptions = {
         tabBarLabel: 'Home',
-        headerTitle: "123456",
-        title: 'Welcome',
         headerStyle: {
             color: 'red'
         },
         header: null,
         tabBarIcon: ({ focused, tintColor }) => (
             <Image
-                style={[styles.icon, { tintColor: focused ? tintColor : 'gray' }]}
+                style={[{ width: 32, height: 32 }, { tintColor: focused ? tintColor : 'gray' }]}
                 source={require('../images/house.png')}
             />
         ),
-    }
-    static defaultNavigationOptions = {
-        title: 'Welcome'
     }
     componentWillMount() {
 
@@ -41,45 +61,138 @@ class Home extends Component {
         console.log(this.props.navigation)
         this.props.dispatch(NavigationActions.navigate({ routeName: 'Detail' }))
     }
+    handleChange(e) {
+        console.log(e)
+    }
+    handleClick() {
+        this.setState({
+            isVisible: true
+        })
+    }
+    onBackdropPress() {
+        console.log(123)
+        this.setState({
+            isVisible: false
+        })
+    }
+    ratingCompleted(rating) {
+        console.log("Rating is: " + rating)
+    }
+    _renderItem({ item, index }) {
+        return <SliderEntry data={item} even={(index + 1) % 2 === 0} />;
+    }
 
-    render() {
+    _renderItemWithParallax({ item, index }, parallaxProps) {
         return (
-            <ImageBackground source={{ uri: 'http://pic15.nipic.com/20110629/5078207_164705330000_2.jpg' }} style={styles.img}>
-                <View style={styles.container}>
-                    <Button text="Goto Detail" onPress={this.gotoDetail} />
-                    <View><Text style={styles.tips}>大鼓米ss线s人民东路店</Text></View>
-                    <Text style={{ color: 'red' }}>
-                        and red
-                </Text>
+            <SliderEntry
+                data={item}
+                even={(index + 1) % 2 === 0}
+                parallax={true}
+                parallaxProps={parallaxProps}
+            />
+        );
+    }
+
+    _renderLightItem({ item, index }) {
+        return <SliderEntry data={item} even={false} />;
+    }
+
+    _renderDarkItem({ item, index }) {
+        return <SliderEntry data={item} even={true} />;
+    }
+    render() {
+        const { users } = this.state
+        return (
+            <View style={styles.img}>
+                {/* <Carousel layout={'tinder'} layoutCardOffset={`9`} />
+                <Carousel
+                    ref={c => this._slider1Ref = c}
+                    data={ENTRIES1}
+                    renderItem={this._renderItemWithParallax}
+                    sliderWidth={sliderWidth}
+                    itemWidth={itemWidth}
+                    hasParallaxImages={true}
+                    firstItem={SLIDER_1_FIRST_ITEM}
+                    inactiveSlideScale={0.94}
+                    inactiveSlideOpacity={0.7}
+                    // inactiveSlideShift={20}
+                    containerCustomStyle={styles.slider}
+                    contentContainerCustomStyle={styles.sliderContentContainer}
+                    loop={true}
+                    loopClonesPerSide={2}
+                    autoplay={true}
+                    autoplayDelay={500}
+                    autoplayInterval={3000}
+                    onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index })}
+                /> */}
+                <View style={styles.exampleContainer}>
+                    <Text style={styles.title}>{`Example ${1}`}</Text>
+                    <Text style={styles.subtitle}>{123}</Text>
+                    <Carousel
+                        ref={c => this._slider1Ref = c}
+                        data={ENTRIES1}
+                        layout={'tinder'}
+                        renderItem={this._renderItemWithParallax}
+                        sliderWidth={sliderWidth}
+                        itemWidth={itemWidth}
+                        hasParallaxImages={true}
+                        firstItem={1}
+                        inactiveSlideScale={0.94}
+                        inactiveSlideOpacity={0.7}
+                        inactiveSlideShift={20}
+                        containerCustomStyle={styles.slider}
+                        contentContainerCustomStyle={styles.sliderContentContainer}
+                        loop={true}
+                        loopClonesPerSide={2}
+                        autoplay={false}
+                        autoplayDelay={500}
+                        autoplayInterval={3000}
+                        onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index })}
+                    />
+                    {/* <Pagination
+                        dotsLength={ENTRIES1.length}
+                        activeDotIndex={slider1ActiveSlide}
+                        containerStyle={styles.paginationContainer}
+                        dotColor={'rgba(255, 255, 255, 0.92)'}
+                        dotStyle={styles.paginationDot}
+                        inactiveDotColor={colors.black}
+                        inactiveDotOpacity={0.4}
+                        inactiveDotScale={0.6}
+                        carouselRef={this._slider1Ref}
+                        tappableDots={!!this._slider1Ref}
+                    /> */}
                 </View>
-            </ImageBackground>
+            </View>
         )
     }
 }
 
-const styles = ScaledSheet.create({
-    img: {
-        flex: 1
-    },
-    container: {
-        width: '1920@s', // = scale(100)
-        height: '288@vs',
-        backgroundColor: 'yellow',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: 10
+// const styles = ScaledSheet.create({
+//     img: {
+//         flex: 1
+//     },
+//     box: {
+//         width: "50%"
+//     },
+//     container: {
+//         width: '1920@s', // = scale(100)
+//         // height: '288@vs',
+//         backgroundColor: 'yellow',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         marginLeft: 'auto',
+//         marginRight: 'auto',
+//         marginTop: 10
 
-    },
-    tips: {
-        fontSize: '40@s',
-        color: 'pink'
-    },
-    icon: {
-        width: 32,
-        height: 32,
-    },
-})
+//     },
+//     tips: {
+//         fontSize: '40@s',
+//         color: 'pink'
+//     },
+//     icon: {
+//         width: 32,
+//         height: 32,
+//     },
+// })
 
 export default Home
---
+++ app/static/entries.js
@@ -0,0 +1,65 @@
+export const ENTRIES1 = [
+    {
+        title: 'Beautiful and dramatic Antelope Canyon',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://ss1.baidu.com/-4o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a9e671b9a551f3dedcb2bf64a4eff0ec/4610b912c8fcc3cef70d70409845d688d53f20f7.jpg'
+    },
+    {
+        title: 'Earlier this morning, NYC',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=342e12b86563f624035d3f03b745eb32/203fb80e7bec54e7f0e0839fb7389b504fc26a27.jpg'
+    },
+    {
+        title: 'White Pocket Sunset',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+        illustration: 'https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=00af05b334f33a87816d061af65d1018/8d5494eef01f3a29f863534d9725bc315d607c8e.jpg'
+    },
+    {
+        title: 'Acrocorinth, Greece',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561469754819&di=74281f779c1f1d675113b60c4d4ec512&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Ff2deb48f8c5494eefd4b47a327f5e0fe99257e1a.jpg'
+    },
+    {
+        title: 'The lone tree, majestic landscape of New Zealand',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561469754819&di=30185f0aeec5bb46eb699a8267e803c2&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2F8b82b9014a90f6030dded4fd3312b31bb051ed1a.jpg'
+    },
+    {
+        title: 'Middle Earth, Germany',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561469754819&di=8dde0ee6b42bbe83a13f848942b9866d&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2F2934349b033b5bb506b868443cd3d539b600bc19.jpg'
+    }
+];
+
+export const ENTRIES2 = [
+    {
+        title: 'Favourites landscapes 1',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/SsJmZ9jl.jpg'
+    },
+    {
+        title: 'Favourites landscapes 2',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://i.imgur.com/5tj6S7Ol.jpg'
+    },
+    {
+        title: 'Favourites landscapes 3',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat',
+        illustration: 'https://i.imgur.com/pmSqIFZl.jpg'
+    },
+    {
+        title: 'Favourites landscapes 4',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://i.imgur.com/cA8zoGel.jpg'
+    },
+    {
+        title: 'Favourites landscapes 5',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/pewusMzl.jpg'
+    },
+    {
+        title: 'Favourites landscapes 6',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat',
+        illustration: 'https://i.imgur.com/l49aYS3l.jpg'
+    }
+];
